3e063999108e3b5d9d97f990f3ff2ccf717002d8
OlegBiletsky <ol.biletsky@gmail.com>
2020-10-21 23:24:58 +0300

    Add description function and refac code

--- index.js
+++ index.js
@@ -1,17 +1,31 @@
-/*1.
-Створити сторінку на якій будемо відображати базовий текст в span("Очікую на ввід даних").
-При натисканні alt + A, замість спана зявиться інпут з аналогічним текстом, який тепер ви зможете відредагувати.
-Далі коли ви натисните shift + S дані збережуться і знов будуть відображатись як span.
-*/
+//
+function description(task_number, task_number_descr) {
+    let main = document.getElementById("main");
+    let p1 = document.createElement('p');
+    p1.textContent =` *** ${task_number} *** ` ;
+    let p2 = document.createElement('p');
+    p2.textContent = `${task_number_descr}`;
+    main.append(p1, p2);
+    let temp = "task"+ (task_number + 1);
+    let taskX = document.getElementById(`${temp}`);
+main.insertBefore(p1, taskX);
+main.insertBefore(p2, taskX);
+}
+//
+
+//1
+let task1_descr = "Створити сторінку на якій будемо відображати базовий текст в span('Очікую на ввід даних'). При натисканні Alt + A, замість спана зявиться інпут з аналогічним текстом, який тепер ви зможете відредагувати. Далі коли ви натисните Shift + S дані збережуться і знов будуть відображатись як span. ///Тут є бага якщо вводити текст і знову нажати Alt + A!!!";
+description(1, task1_descr);
+
 hard_string = "Очікую на ввід даних...";
 
 let hard_span = document.createElement('span');
 hard_span.textContent = hard_string;
 
 let input = document.createElement('input');
+let task2 = document.getElementById('task2');
+main.insertBefore(hard_span, task2)
 
-let main = document.getElementById("main");
-main.append(hard_span);
 
 // text_flag = ["Щоб змінити цей напис натисніть Alt+A","Щоб зберегти зміни натисніть Shift+S"];
 // let description = document.createElement('p');
@@ -31,7 +45,8 @@ document.addEventListener('keydown', function(e){
         let temp = hard_span.textContent;
         hard_span.remove();
         input.value = temp;
-        main.append(input);
+        let task2 = document.getElementById('task2');
+        main.insertBefore(input, task2);
     }
 });
 
@@ -42,29 +57,31 @@ document.addEventListener('keydown', function(e){
         let temp = input.value;
         input.remove();
         hard_span.textContent = temp;
-        main.append(hard_span);
+        let task2 = document.getElementById('task2');
+        main.insertBefore(hard_span, task2);
     }
 });
 
 
-/*
-2. Повісити івент лісенер на ресайз сторінки, коли розмір сторінки буде менше 600px , виводити лог ми векористовуємо мобільну версію, при більшому розмірі сторінки - що ми використовуємо десктоп версію.
-*/
-window.addEventListener('resize', myResizeFunction);
+//2
+let task2_descr = "Повісити івент лісенер на ресайз сторінки, коли розмір сторінки буде менше 600px , виводити лог ми векористовуємо мобільну версію, при більшому розмірі сторінки - що ми використовуємо десктоп версію.";
+description(2, task2_descr);
 
+window.addEventListener('resize', myResizeFunction);
 
 function myResizeFunction () {
     if (innerWidth < 600) {
-        console.log("Ми використовуємо мобільну версію");
+        console.log("Desctop version");
     } else {
-        console.log("Ми використовуємо десктоп версію")
+        console.log("Mobile version")
     }
 }
 
 
-/*
-3. Повісити івент на інпут, при зміні стану інпута - виводити повідомленням зі значенням яке зараз є введено.
-*/
+//3
+let task3_descr = "Повісити івент на інпут, при зміні стану інпута - виводити повідомленням зі значенням яке зараз є введено."
+description(3, task3_descr);
+
 let newInput = document.createElement('input');
 main.append(newInput);
 
@@ -78,9 +95,11 @@ function inputChangeFunction() {
 
 
 
-/*
-4. При натисканні на кнопку альт+ о виводити модальне вікно на екран, альт + с - закривати.
-*/
-/*
-5. Створити модальне відкно аналог Алерта але з гарним дизайном. по кліку на кнопку зявляється модальне вікно по центрі екрану(центрування елемента). В модальному вікні має бути текст та кнопка закрити.
-*/
+
+//4
+let task4_descr = "При натисканні на кнопку альт + o виводити модальне вікно на екран, альт + с - закривати."
+description(4, task4_descr);
+
+//5
+let task5_descr = "Створити модальне відкно аналог Алерта але з гарним дизайном. по кліку на кнопку зявляється модальне вікно по центрі екрану(центрування елемента). В модальному вікні має бути текст та кнопка закрити."
+description(5, task5_descr);
